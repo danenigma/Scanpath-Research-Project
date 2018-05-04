324d5b897cb6974f1ffdb203ce2dd388e314246a
danenigma <danielfekaduenigma@yahoo.com>
2018-05-04 23:23:25 +0300

    training with saliency

--- models.py
+++ models.py
@@ -30,6 +30,38 @@ class EncoderCNN(nn.Module):
         features = self.bn(self.linear(features))
         return features
     
+class ResNet50(nn.Module):
+
+	def __init__(self, embed_size):
+		super(ResNet50, self).__init__()
+		
+		model = models.resnet50(pretrained=False)
+		modified_model = list(model.children())
+		modified_model[0] = nn.Conv2d(1, 64, 
+									  kernel_size=(7, 7), 
+									  stride=(2, 2), 
+									  padding=(3, 3),
+									  bias=False) 
+		
+		self.modified_model = nn.Sequential(*(modified_model[:-1]))
+		self.linear = nn.Linear(2048, embed_size)
+		self.bn     = nn.BatchNorm1d(embed_size, momentum=0.01)
+		self.init_weights()
+	
+	def init_weights(self):
+		"""Initialize the weights."""
+		self.linear.weight.data.normal_(0.0, 0.02)
+		self.linear.bias.data.fill_(0)
+		
+	def forward(self, images):
+		"""Extract the image feature vectors."""
+		features = self.modified_model(images)
+		features = Variable(features.data)
+		features = features.view(features.size(0), -1)
+		features = self.bn(self.linear(features))
+		
+		return features
+
     
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers):
--- playing.py
+++ playing.py
@@ -3,20 +3,37 @@ import torch.nn as nn
 import torchvision.models as models
 from torch.autograd import Variable
 
-class VGG16(nn.Module):
+class ResNet50(nn.Module):
 
-    def __init__(self):
-        """Load the pretrained VGG-16 and replace top fc layer."""
-        super(VGG16, self).__init__()
-        vgg = models.vgg16(pretrained=True)
-        self.model = nn.Sequential(*(self.vgg.features[i] for i in range(29)))
-       
-    def forward(self, images):
-        """Extract the image feature vectors."""
-        features = self.self.model(images)
-
-        return features
+	def __init__(self, embed_size):
+		"""Load the pretrained VGG-16 and replace top fc layer."""
+		super(ResNet50, self).__init__()
+		model = models.resnet50(pretrained=False)
+		modified_model = list(model.children())
+		modified_model[0] = nn.Conv2d(1, 64, 
+									  kernel_size=(7, 7), 
+									  stride=(2, 2), 
+									  padding=(3, 3),
+									  bias=False) 
+		self.modified_model = nn.Sequential(*(modified_model[:-1]))
+		self.linear = nn.Linear(2048, embed_size)
+		self.bn     = nn.BatchNorm1d(embed_size, momentum=0.01)
+		self.init_weights()
+	
+	def init_weights(self):
+		"""Initialize the weights."""
+		self.linear.weight.data.normal_(0.0, 0.02)
+		self.linear.bias.data.fill_(0)
+		
+	def forward(self, images):
+		"""Extract the image feature vectors."""
+		features = self.modified_model(images)
+		features = Variable(features.data)
+		features = features.view(features.size(0), -1)
+		features = self.bn(self.linear(features))
+		return features
+		
 if __name__=='__main__':
-	vgg = VGG16()
-	x   = Variable(torch.rand(1, 3, 224, 224))
-	print('out: ', vgg.model(x).shape)
+	resnet = ResNet50(512)
+	x      = Variable(torch.rand(2, 1, 224, 224))
+	print('out: ', resnet(x).shape)
